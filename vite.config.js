be0111b38ac4e2a68f73565f71b4f772f31b05e0
Sebastian Marquez <sebastianmarquez6a@gmail.com>
2023-09-09 18:45:11 -0400

    [fix] fixing vitejs config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
-dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: process.env.VITE_HOST || "0.0.0.0",
-    port: process.env.VITE_PORT || 5173,
+    host: true,
+    port: process.env.VITE_PORT,
   },
   plugins: [react()],
 });
